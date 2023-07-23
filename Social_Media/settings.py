dbad100fd269f58692a2cd1d02ef71adbbf49422
Pranav Ambadkar <125912673+pranavambadkar@users.noreply.github.com>
2023-07-23 13:35:38 +0530

    Update settings.py

--- Social_Media/settings.py
+++ Social_Media/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'netbuzz/media')
 MEDIA_URL = '/media/'
 
